f2bf186ea6054583fa3ce0acaf945492f5c6ee47
Pablo Robalino <pjrobalinol@utn.edu.ec>
2021-06-14 13:51:03 -0500

    fuentePago retorna cuentaBancaria en Cabeceracreate y CabeceraEdit

--- src/components/Cabecera/CabeceraCreate.jsx
+++ src/components/Cabecera/CabeceraCreate.jsx
@@ -6,11 +6,14 @@ import {stringify} from "querystring"
 
 const CabeceraCreate = () => {
     const url = "https://apirestcxp.herokuapp.com/cxp/cabecera/"
+    const urlFuente = "https://apirestcxp.herokuapp.com/cxp/fuentepago/"
     const urlProv = "https://app-mod-com.herokuapp.com/compras/facturaProveedor"
 
     const history = useHistory()
 
     const [proveedores, setProveedores] = useState(["w","s"])
+    const [fuente, setFuente] = useState([0])
+    const [cuentaBancariaRO, setCuentabancariaRO] = useState("No se ha seleccionado una fuente de pago.")
 
     const hoy = new Date()
 
@@ -59,6 +62,25 @@ const CabeceraCreate = () => {
         getProveedores()
     }, [])
 
+    useEffect(() => {
+        const getFuentes = async () => {
+            const consulta = await axios.get(urlFuente)
+            if (consulta.data.code === 0){}
+            else {
+                setFuente(consulta.data || [0])
+            }
+        }
+        getFuentes()
+    }, [])
+
+    const cuentaBancaria = (val) => {
+        try {
+            setCuentabancariaRO(fuente[val-1].cuentabancaria)
+        } catch (error) {
+            setCuentabancariaRO("No se ha seleccionado una fuente de pago.")
+        }
+    }
+
     const { descripcionpago, prov_dni, idfuente, fechapago } = cabeceraForm;
 
     return (
@@ -86,9 +108,25 @@ const CabeceraCreate = () => {
                     </div>
                     <div className="input-group mb-2 bg-dark">
                         <label className="col-2 align-self-center aND text-center m-2">Id Fuente de Pago</label>
-                        <input className="form-control" value={idfuente} 
-                            onChange={(e) => setCabeceraForm({...cabeceraForm,idfuente: e.target.value})}
-                            required />
+                        <div className="col row bg-white">
+                            <div className="col-2">
+                                <select className="col form-select" value={idfuente}
+                                    onChange={(e) => {
+                                        setCabeceraForm({ ...cabeceraForm, idfuente: e.target.value })
+                                        cuentaBancaria(e.target.value)
+                                    }} >
+                                        <option>0</option>
+                                    {fuente.map((fuent, index) => {
+                                        return (
+                                            <option key={index}>{fuent.idfuente}</option>
+                                        )
+                                    })}
+                                </select>
+                            </div>
+                            <div className="col">
+                                <input className="col-2 form-control" value={cuentaBancariaRO} readOnly></input>
+                            </div>
+                        </div>
                     </div>
                     <div className="input-group mb-2 bg-dark">
                         <label className="col-2 align-self-center aND text-center m-2">Fecha</label>
